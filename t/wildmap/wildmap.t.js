a57e20c60097259a4222c0a705d74c563f8c5d07
Alan Gutierrez <alan@prettyrobots.com>
2015-11-24 22:20:10 -0600

    Implement wildcard path map.

--- t/wildmap/wildmap.t.js
+++ t/wildmap/wildmap.t.js
@@ -1,5 +1,38 @@
-require('proof')(1, prove)
+require('proof')(10, prove)
 
 function prove (assert) {
-    assert(require('../..'), 'require')
+    var WildMap = require('../..')
+
+    var map = new WildMap
+
+    map.add('.hello.world'.split('.'), 'a')
+
+    assert(map.get('.hello.world'.split('.')), [ 'a' ], 'get')
+    assert(map.get('.hello'.split('.')), [], 'get empty parent')
+    assert(map.get('.hello.earth'.split('.')), [], 'get null path')
+
+    assert(map.match('.hello.world'.split('.')), [ 'a' ], 'get')
+
+    map.add('.hello'.split('.'), 'c')
+    map.add('.hello.*'.split('.'), 'd')
+    map.add('.hello.world'.split('.'), 'b')
+
+    assert(map.match('.hello.world'.split('.')), [ 'c', 'a', 'b', 'd' ], 'match many')
+    assert(map.match('.hello.earth'.split('.')), [ 'c', 'd' ], 'match wild')
+
+    map.add('.'.split('.'), 'x')
+    assert(map.match('.hello.earth'.split('.')), [ 'x', 'c', 'd' ], 'add root')
+
+    // remove from non-existant path
+    map.remove('.hello.earth'.split('.'), 'z')
+
+    // remove item not in list at path
+    map.remove('.hello'.split('.'), 'y')
+
+    map.remove('.hello'.split('.'), 'c')
+    assert(map.match('.hello.earth'.split('.')), [ 'x', 'd' ], 'remove value from parent')
+    map.remove('.'.split('.'), 'x')
+    assert(map.match('.hello.earth'.split('.')), [ 'd' ], 'remove value from root')
+    map.remove('.hello.*'.split('.'), 'd')
+    assert(map.match('.hello.world'.split('.')), [ 'a', 'b' ], 'remove leaf')
 }
--- wildmap.js
+++ wildmap.js
@@ -1,1 +1,89 @@
-module.exports=1
+function WildMap () {
+    this._root = {}
+    this._root[''] = this._root
+}
+
+WildMap.prototype.add = function (path, value) {
+    var node = this._root
+
+    for (var i = 0, I = path.length; i < I; i++) {
+        if (!node[path[i]]) {
+            node[path[i]] = {}
+        }
+        node = node[path[i]]
+    }
+
+    var values = node['.values']
+    if (values == null) {
+        values =  node['.values'] = []
+    }
+
+    values.push(value)
+}
+
+WildMap.prototype.remove = function (path, value) {
+    var node = this._root, parent
+
+    for (var i = 0, I = path.length; i < I; i++) {
+        if (!node[path[i]]) {
+            return
+        }
+        parent = node
+        node = node[path[i]]
+    }
+
+
+    var values = node['.values'], index = values.indexOf(value)
+    if (index != -1) {
+        values.splice(index, 1)
+    }
+
+    if (values.length == 0) {
+        delete node['.values']
+        if (I > 1 && Object.keys(node).length == 0) {
+            delete parent[path[I - 1]]
+        }
+    }
+}
+
+WildMap.prototype.get = function (path) {
+    var node = this._root, i = 0, values
+    for (var i = 0, I = path.length; i < I; i++) {
+        node = node[path[i]]
+        if (node == null) {
+            return []
+        }
+    }
+    return node['.values'] || []
+}
+
+WildMap.prototype.match = function (path) {
+    var parents = [ this._root ], i = 0, array = [],
+        parent, children, node, values, j, J, k, K
+    while (i < path.length && parents.length != 0) {
+        children = []
+        while (parents.length) {
+            parent = parents.shift()
+            node = parent[path[i]]
+            if (node != null) {
+                children.push(node)
+            }
+            node = parent['*']
+            if (node != null) {
+                children.push(node)
+            }
+        }
+        for (j = 0, J = children.length; j < J; j++) {
+            if ((values = children[j]['.values']) != null) {
+                for (k = 0, K = values.length; k < K; k++) {
+                    array.push(values[k])
+                }
+            }
+        }
+        parents = children
+        i++
+    }
+    return array
+}
+
+module.exports = WildMap
